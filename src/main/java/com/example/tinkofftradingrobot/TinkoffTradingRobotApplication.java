69927c280a3030b661f67a962d78721be124857a
Pavel Tabakov <ptabakov@intetech.ru>
2022-04-28 11:51:42 +0300

    Initial commit

---
+++ src/main/java/com/example/tinkofftradingrobot/TinkoffTradingRobotApplication.java
@@ -0,0 +1,13 @@
+package com.example.tinkofftradingrobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinkoffTradingRobotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinkoffTradingRobotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tinkofftradingrobot/TinkoffTradingRobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tinkofftradingrobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinkoffTradingRobotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
