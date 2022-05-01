3b27224e770458947585db4f0e01a7f32f8fe157
Pavel Tabakov <ptabakov@intetech.ru>
2022-05-01 07:06:35 +0300

    added OrderRepo.java

---
+++ src/main/java/com/example/tinkofftradingrobot/repository/OrderRepo.java
@@ -0,0 +1,8 @@
+package com.example.tinkofftradingrobot.repository;
+
+import com.example.tinkofftradingrobot.model.OrderEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepo extends JpaRepository<OrderEntity, String> {
+
+}
