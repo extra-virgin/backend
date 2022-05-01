daab80df4a390b47f791b6e2a8fb183f2f9835b2
Pavel Tabakov <ptabakov@intetech.ru>
2022-05-01 07:06:27 +0300

    added OrderEntity.java

---
+++ src/main/java/com/example/tinkofftradingrobot/model/OrderEntity.java
@@ -0,0 +1,56 @@
+package com.example.tinkofftradingrobot.model;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+import ru.tinkoff.piapi.contract.v1.OrderDirection;
+import ru.tinkoff.piapi.contract.v1.OrderType;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "order")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class OrderEntity {
+
+    @Id
+    @Column(name = "order_id")
+    private String orderId;
+
+    @Column(name = "figi")
+    String figi;
+
+    @Column(name = "price")
+    Long price;
+
+    @Column(name = "account_id")
+    String accountID;
+
+    @Column(name = "quantity")
+    int quantity;
+
+    @Column(name = "direction")
+    @Enumerated(EnumType.STRING)
+    OrderDirection direction;
+
+    @Column(name = "type")
+    @Enumerated(EnumType.STRING)
+    OrderType type;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        OrderEntity that = (OrderEntity) o;
+        return orderId != null && Objects.equals(orderId, that.orderId);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
