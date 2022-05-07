abe71a6aa3b83d5b5ab5905fd50a6c0147e343bc
Nikita Kulakov <Nikitka.Kulakov@yandex.ru>
2022-05-07 17:18:18 +0300

    refactor: rename validators, converters

---
+++ src/main/java/com/example/tinkofftradingrobot/config/preload/LoadDatabase.java
@@ -0,0 +1,2 @@
+package com.example.tinkofftradingrobot.config.preload;public class LoadDatabase {
+}
---
+++ src/main/java/com/example/tinkofftradingrobot/repository/UserRepo.java
@@ -0,0 +1,2 @@
+package com.example.tinkofftradingrobot.repository;public interface UserRepo {
+}
