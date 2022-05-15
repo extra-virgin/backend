16d870a3952034b21c6c52c23e5fef4349ef704a
Pavel Tabakov <ptabakov@intetech.ru>
2022-05-16 00:45:37 +0300

    update SolutionRequest for resolve() needles

--- src/main/java/com/example/tinkofftradingrobot/strategy/solution/data/SolutionRequest.java
+++ src/main/java/com/example/tinkofftradingrobot/strategy/solution/data/SolutionRequest.java
@@ -1,4 +1,15 @@
 package com.example.tinkofftradingrobot.strategy.solution.data;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SolutionRequest {
+    String accountID;
+    String figi;
 }
