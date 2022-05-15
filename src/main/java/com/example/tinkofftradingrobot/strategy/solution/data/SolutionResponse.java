fcab75f9ff5ec040dea9ad353c5120f7d14a732b
Pavel Tabakov <ptabakov@intetech.ru>
2022-05-16 00:45:48 +0300

    update SolutionResponse for resolve() needles

--- src/main/java/com/example/tinkofftradingrobot/strategy/solution/data/SolutionResponse.java
+++ src/main/java/com/example/tinkofftradingrobot/strategy/solution/data/SolutionResponse.java
@@ -1,4 +1,19 @@
 package com.example.tinkofftradingrobot.strategy.solution.data;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.tinkoff.piapi.contract.v1.OrderDirection;
+import ru.tinkoff.piapi.contract.v1.OrderType;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SolutionResponse {
+    OrderDirection orderDirection;
+    int quantity;
+    String accountID;
+    String figi;
+    OrderType orderType;
+    String orderID;
 }
