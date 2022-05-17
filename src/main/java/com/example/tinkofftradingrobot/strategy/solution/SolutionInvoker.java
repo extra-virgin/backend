6c53e2d858686652a9968e1a5b8a0390e65837a7
Pavel Tabakov <ptabakov@intetech.ru>
2022-05-18 00:45:31 +0300

    add runStub

--- src/main/java/com/example/tinkofftradingrobot/strategy/solution/SolutionInvoker.java
+++ src/main/java/com/example/tinkofftradingrobot/strategy/solution/SolutionInvoker.java
@@ -1,15 +1,20 @@
 package com.example.tinkofftradingrobot.strategy.solution;
 
+import com.example.tinkofftradingrobot.model.AccountEntity;
 import com.example.tinkofftradingrobot.repository.AccountRepo;
 import com.example.tinkofftradingrobot.repository.OrderRepo;
 import com.example.tinkofftradingrobot.strategy.Strategy;
 import com.example.tinkofftradingrobot.strategy.connection.ConnectionHandler;
+import com.example.tinkofftradingrobot.strategy.solution.data.SolutionRequest;
 import com.example.tinkofftradingrobot.strategy.solution.maker.SolutionMaker;
 import com.example.tinkofftradingrobot.strategy.solution.maker.StubSolutionMaker;
 import org.springframework.stereotype.Component;
+import ru.tinkoff.piapi.core.InvestApi;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Component
 public class SolutionInvoker {
@@ -30,7 +35,28 @@ public class SolutionInvoker {
     }
 
     public void runStub() {
-
+        StubSolutionMaker stubSolutionMaker = (StubSolutionMaker) solutionMakers.get(Strategy.STUB);
+        AtomicReference<SolutionRequest> solutionRequest = new AtomicReference<>();
+        Optional<InvestApi> connection = connectionHandler.getConnectionListSandbox().stream().filter(
+                api -> {
+                    return api.getUserService().getAccountsSync().stream()
+                            .anyMatch(account -> {
+                                if(accountRepo.existsByAccountId(account.getId())) {
+                                    Optional<AccountEntity> accountEntity = accountRepo.findAccountEntityByAccountId(account.getId());
+                                    if (accountEntity.isPresent()) {
+                                        solutionRequest.set(new SolutionRequest());
+                                        solutionRequest.get().setAccountID(accountEntity.get().getAccountId());
+                                        solutionRequest.get().setFigi(accountEntity.get().getFigis());
+                                        return true;
+                                    }
+                                }
+                                return false;
+                            });
+                }
+        ).findFirst();
+        if (connection.isPresent() && !solutionRequest.get().getAccountID().isEmpty()) {
+            stubSolutionMaker.resolve(connection.get(), solutionRequest.get());
+        }
     }
 
 
